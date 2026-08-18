6ef3c4c4be7932681e9ad8220f1102179cfc9bde
SAEZ1205 <saez123m@gmail.com>
2026-08-18 17:31:36 -0500

    Activa contenido de Nosotros Comunidad y Eventos en React

--- cct-site-main/src/features/site/init.ts
+++ cct-site-main/src/features/site/init.ts
@@ -1,5 +1,13 @@
 import { initNavigation } from './navigation';
+import { initRevealMotion } from './reveal';
 import { initInicio } from '../inicio/init';
 import { initNosotros } from '../nosotros/init';
 import { initFormation } from '../formation/init';
-export function initSite(){initNavigation();initInicio();initNosotros();initFormation();}
+
+export function initSite(){
+  initNavigation();
+  initInicio();
+  initNosotros();
+  initFormation();
+  initRevealMotion();
+}
