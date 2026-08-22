7b6dfd24c9e9288d59fdc0a26fc43499254dd1be
SAEZ1205 <saez123m@gmail.com>
2026-08-22 16:03:44 -0500

    Incluye Comunidad y Eventos en build Vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { viteStaticCopy } from 'vite-plugin-static-copy';
-
 export default defineConfig({
   base: process.env.GITHUB_ACTIONS ? '/CCT-PAGE/' : '/',
-  plugins: [
-    react(),
-    viteStaticCopy({
-      targets: [
-        { src: 'assets/**/*', dest: 'assets' },
-        { src: 'site.js', dest: '' },
-        { src: 'career.js', dest: '' },
-        { src: 'calendar.js', dest: '' },
-        { src: 'nosotros.js', dest: '' },
-        { src: 'formation.js', dest: '' },
-        { src: 'course.html', dest: '' },
-        { src: 'course.css', dest: '' },
-        { src: 'course.js', dest: '' }
-      ]
-    })
-  ]
+  plugins:[react(),viteStaticCopy({targets:[
+    {src:'assets/**/*',dest:'assets'},
+    {src:'site.js',dest:''},{src:'career.js',dest:''},{src:'calendar.js',dest:''},
+    {src:'nosotros.js',dest:''},{src:'formation.js',dest:''},{src:'community.js',dest:''},{src:'events.js',dest:''},
+    {src:'course.html',dest:''},{src:'course.css',dest:''},{src:'course.js',dest:''}
+  ]})]
 });
