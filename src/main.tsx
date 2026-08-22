cdadadc3272926798def224b0dcd3f5a314e2d19
SAEZ1205 <saez123m@gmail.com>
2026-08-22 17:20:12 -0500

    Importa estilos canónicos por sección

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,10 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import '../styles.css';
 import './styles/tailwind.css';
-import './styles/compat-fixes.css';
+import './styles/sections/nosotros.css';
+import './styles/sections/formacion.css';
+import './styles/sections/comunidad.css';
+import './styles/sections/eventos.css';
 import App from './App';
 
 const root = document.getElementById('root');
